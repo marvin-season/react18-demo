1f8a4dd986607ae893fd4bdbb519b83f55e7e6e9
marvin <marvin010528@foxmail.com>
2023-07-16 08:29:56 +0800

    feat: embed jira-service

--- __json_server_mock__/middleware.js
+++ __json_server_mock__/middleware.js
@@ -1,9 +1,13 @@
 module.exports = (req, res, next) => {
   if (req.method == "POST" && req.path == "/login") {
-    return res.status(200).json({
-      user: {
-        token: "Bodsa dbuice==",
-      },
-    });
+    if (req.body.username == "aaa") {
+      return res.status(200).json({
+        user: {
+          token: "Bodsa dbusice==",
+        },
+      });
+    } else {
+      return res.status(400).json();
+    }
   }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import UserScreen from "views/user-screen";
 import { LoginScreen } from "screens/login";
-
 function App() {
   return (
     <div className="App">
